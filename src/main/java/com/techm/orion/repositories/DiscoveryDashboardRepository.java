96abb4bfc88260162f9ce8c34ea7c6144c0b8e4d
Navya Reddy <br00556618@techmahindra.com>
2020-09-21 12:31:51 +0530

    removed unused methods

--- src/main/java/com/techm/orion/repositories/DiscoveryDashboardRepository.java
+++ src/main/java/com/techm/orion/repositories/DiscoveryDashboardRepository.java
@@ -16,13 +16,7 @@ public interface DiscoveryDashboardRepository extends JpaRepository<DiscoveryDas
 
 	Set<DiscoveryDashboardEntity> findByDisStatusIgnoreCase(String status);
 
-	Set<DiscoveryDashboardEntity> findByDisStatusIgnoreCaseAndDisCreatedByIgnoreCase(String status,	String user);	
-	
-	@Query(value = "select * from c3p_t_discovery_dashboard where dis_dash_id=:disDashId", nativeQuery = true)
-	DiscoveryDashboardEntity findAllBydashId(@Param("disDashId") String disDashId);
-	
-	@Query(value = "select * from c3p_t_discovery_dashboard where dis_dash_id=:disDashId", nativeQuery = true)
-	Set<String> findAllById(@Param("disDashId") String disDashId);
+	Set<DiscoveryDashboardEntity> findByDisStatusIgnoreCaseAndDisCreatedByIgnoreCase(String status,	String user);
 	
 	@Query(value = "select count(dis_status) from c3p_t_discovery_dashboard where dis_status=:status and dis_created_by like :creatorName ", nativeQuery = true)
 	int getRequestStatusCount(@Param("status") String status, @Param("creatorName") String creatorName);
