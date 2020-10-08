369d00752d43301d85e61e288981fbc55db32fd7
RS00502123 <RS00502123@INPUHJPC04988.TechMahindra.com>
2020-10-08 15:35:40 +0530

    Master Feature entities, repostories and controller

---
+++ src/main/java/com/techm/orion/repositories/MasterCharacteristicsRepository.java
@@ -0,0 +1,20 @@
+package com.techm.orion.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.techm.orion.entitybeans.DeviceDiscoveryEntity;
+import com.techm.orion.entitybeans.MasterCharacteristicsEntity;
+import com.techm.orion.entitybeans.SiteInfoEntity;
+
+@Repository
+@Transactional
+public interface MasterCharacteristicsRepository extends JpaRepository<MasterCharacteristicsEntity, Long> {
+
+}
