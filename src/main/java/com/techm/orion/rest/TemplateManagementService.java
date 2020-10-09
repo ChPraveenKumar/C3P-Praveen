40d2a07ee280e226fd8e2ec0266d77e3ffe08095
Anjireddy Reddem <anjireddy.reddem@techmahindra.com>
2020-10-09 15:43:47 +0530

    Defect fix 1987

--- src/main/java/com/techm/orion/rest/TemplateManagementService.java
+++ src/main/java/com/techm/orion/rest/TemplateManagementService.java
@@ -219,13 +219,13 @@ public class TemplateManagementService implements Observer {
 			JSONObject json = (JSONObject) parser.parse(newFeature);
 
 			AddNewFeatureTemplateMngmntPojo addNewFeatureTemplateMngmntPojo = new AddNewFeatureTemplateMngmntPojo();
-			String templateAndVesion = json.get("templateid").toString() + "_v"
+			String templateAndVesion = json.get("templateid").toString() + "_V"
 					+ json.get("templateVersion").toString();
 			boolean ifTemplateAlreadyPresent = templateDao.checkTemplateVersionAlredyexist(templateAndVesion);
 			if (ifTemplateAlreadyPresent) {
 				double value = Double.parseDouble(json.get("templateVersion").toString());
 				value = value + 0.1;
-				templateAndVesion = json.get("templateid").toString() + "_v" + numberFormat.format(value);
+				templateAndVesion = json.get("templateid").toString() + "_V" + numberFormat.format(value);
 				templateId = json.get("templateid").toString();
 				templateVersion = numberFormat.format(value);
 				addNewFeatureTemplateMngmntPojo.setTemplateid(templateAndVesion);
@@ -318,7 +318,7 @@ public class TemplateManagementService implements Observer {
 			templateDao.updateTransactionCommandForNewTemplate(addNewFeatureTemplateMngmntPojo);
 
 			response = templateSaveFlowService.saveConfigurationTemplate(newFeature, templateId, templateVersion);
-			camundaService.initiateApprovalFlow(templateAndVesion, templateVersion, "Admin");
+			camundaService.initiateApprovalFlow(templateId, templateVersion, "Admin");
 
 		} catch (Exception e) {
 			e.printStackTrace();
