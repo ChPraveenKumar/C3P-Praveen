1fadc0f3cdc2da2d39bef7dbc10e5eb01404c77d
Dhanshri Ravsaheb Mane <DM00631464@techmahindra.com>
2020-11-03 12:28:15 +0530

    Discrepancy Method Name Change

--- src/main/java/com/techm/orion/repositories/MasterOIDRepository.java
+++ src/main/java/com/techm/orion/repositories/MasterOIDRepository.java
@@ -13,9 +13,9 @@ import com.techm.orion.entitybeans.MasterOIDEntity;
 public interface MasterOIDRepository extends JpaRepository<MasterOIDEntity, Long> {
 
 	MasterOIDEntity findByOidNoAndOidVendorAndOidNetworkType(String oid,String vendor,String networkType);
-
+	
 	@Query(value = "SELECT oid_m_display_name FROM c3p_m_oid_master_info where oid_m_no = :oidNo and oid_m_for_vendor= :vendor ", nativeQuery = true)
-	String findOidNo(@Param("oidNo") String oidNo, @Param("vendor") String vendor);
+	String findOidDisplayName(@Param("oidNo") String oidNo, @Param("vendor") String vendor);
 
 	@Query(value = "SELECT * FROM c3p_m_oid_master_info where oid_m_category='Interface' and oid_m_for_vendor=:oidVendor and oid_m_network_type=:oidNetworkType and oid_m_default_flag='Y'", nativeQuery = true)
 	List<MasterOIDEntity> findOidAndDisplayName(@Param("oidVendor") String oidVendor,
