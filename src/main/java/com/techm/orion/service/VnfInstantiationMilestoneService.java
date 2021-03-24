521ca3a32b3d91b44572bd8c72bc6ed107d39040
Anjireddy Reddem <anjireddy.reddem@techmahindra.com>
2021-03-24 20:47:56 +0530

    OpenStack VNF instantiation service change

--- src/main/java/com/techm/orion/service/VnfInstantiationMilestoneService.java
+++ src/main/java/com/techm/orion/service/VnfInstantiationMilestoneService.java
@@ -1,55 +1,50 @@
 package com.techm.orion.service;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-import org.json.JSONException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.techm.orion.utility.TSALabels;
-
+@Service
 public class VnfInstantiationMilestoneService {
+	private static final Logger logger = LogManager.getLogger(VnfInstantiationMilestoneService.class);
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	@SuppressWarnings("unchecked")
-	public JSONObject callPython(String requestId, String version) throws IOException, JSONException, ParseException {
+	public boolean vnfInstantiation(String requestId, String version) {
+		logger.info("Start - vnfInstantiation");
+		boolean vnfInstantiated = false;
+		HttpHeaders headers = null;
+		JSONObject reqInstatiation = null;
+		JSONParser jsonParser = null;
+		try {
+			headers = new HttpHeaders();
+			reqInstatiation = new JSONObject();
+			jsonParser = new JSONParser();
+			reqInstatiation.put(new String("requestId"), requestId);
+			reqInstatiation.put(new String("version"), version);
+			HttpEntity<JSONObject> entity = new HttpEntity<JSONObject>(reqInstatiation, headers);
+			String url = TSALabels.PYTHON_SERVICES.getValue() + "C3P/api/ResourceFunction/Cloud/compute/instances/";
+			String response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class).getBody();
+			JSONObject responseJson = (JSONObject) jsonParser.parse(response);
+			if (responseJson.containsKey("workflow_status") && responseJson.get("workflow_status") != null
+					&& "true".equalsIgnoreCase(responseJson.get("workflow_status").toString())) {
+				vnfInstantiated = true;
+			}
 
-		String serverPath= TSALabels.PYTHON_SERVICES.getValue();
-		String query = serverPath + "C3P/api/ResourceFunction/GCP/compute/instances/";
-
-		JSONObject obj = new JSONObject();
-
-		obj.put(new String("requestId"), requestId);
-
-		obj.put(new String("version"), version);
-
-
-		URL url = new URL(query);
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setConnectTimeout(5000);
-		conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-		conn.setDoOutput(true);
-		conn.setDoInput(true);
-		conn.setRequestMethod("POST");
-
-		OutputStream os = conn.getOutputStream();
-		os.write(obj.toString().getBytes("UTF-8"));
-		os.close();
-
-		// read the response
-		InputStream in = new BufferedInputStream(conn.getInputStream());
-		String result = org.apache.commons.io.IOUtils.toString(in, "UTF-8");
-		//String result="{\"output\": true}";
-		JSONObject jsonObject = new JSONObject();
-		JSONParser parser = new JSONParser();  
-		jsonObject = (JSONObject) parser.parse(result);  
-		//in.close();
-		//conn.disconnect();
-		return jsonObject;
+		} catch (ParseException exe) {
+			logger.info("Exception - " + exe.getMessage());
+		}
+		logger.info("End - vnfInstantiation - vnfInstantiated ->" + vnfInstantiated);
+		return vnfInstantiated;
 	}
 }
