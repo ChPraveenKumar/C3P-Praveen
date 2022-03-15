4ea3a8a19160a808c2b08c37ef1577bb2303fbfb
Saurabh Mahadev Chavane <SC00775438@techmahindra.com>
2022-03-15 17:44:02 +0530

    R22.03.SP.05_Saurabh

--- src/main/java/com/techm/c3p/core/service/CertificationTestFlagDetailsService.java
+++ src/main/java/com/techm/c3p/core/service/CertificationTestFlagDetailsService.java
@@ -1,10 +1,7 @@
 package com.techm.c3p.core.service;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import com.techm.c3p.core.dao.RequestInfoDao;
 import com.techm.c3p.core.pojo.CertificationTestPojo;
 import com.techm.c3p.core.pojo.CreateConfigRequestDCM;
 
@@ -12,44 +9,38 @@ import com.techm.c3p.core.pojo.CreateConfigRequestDCM;
 public class CertificationTestFlagDetailsService {
 
 	@Autowired
-	private RequestInfoDao requestInfoDao;
-	
-	public CertificationTestPojo getPrevalidationTestFlag(CreateConfigRequestDCM createConfigRequestDCM) throws Exception
-	{
-	    
-		String requestId=createConfigRequestDCM.getRequestId();
-		
-		String TestType=createConfigRequestDCM.getTestType();
-		
-		String version=createConfigRequestDCM.getVersion_report();
-		CertificationTestPojo certificationTestPojo=new CertificationTestPojo();
-				
-		try{
+	private RequestInfoService requestInfoService;
 
-			if(!version.contains("."))
-			{
-				version=version+".0";
+	public CertificationTestPojo getPrevalidationTestFlag(CreateConfigRequestDCM createConfigRequestDCM)
+			throws Exception {
+
+		String requestId = createConfigRequestDCM.getRequestId();
+
+		String TestType = createConfigRequestDCM.getTestType();
+
+		String version = createConfigRequestDCM.getVersion_report();
+		CertificationTestPojo certificationTestPojo = new CertificationTestPojo();
+
+		try {
+
+			if (!version.contains(".")) {
+				version = version + ".0";
 			}
-		
-		if(TestType.equalsIgnoreCase("preValidate"))
-		{
-			certificationTestPojo=requestInfoDao.getCertificationTestFlagData(requestId,version,TestType);
-		}
-		
-		if(TestType.equalsIgnoreCase("networkTest"))
-		{
-			certificationTestPojo=requestInfoDao.getCertificationTestFlagData(requestId,version,TestType);
-		}
-		if(TestType.equalsIgnoreCase("HealthTest"))
-		{
-			certificationTestPojo=requestInfoDao.getCertificationTestFlagData(requestId,version,TestType);
-		}
-		
-	    }
-	    catch (Exception ex) {
+
+			if (TestType.equalsIgnoreCase("preValidate")) {
+				certificationTestPojo = requestInfoService.getCertificationTestFlagData(requestId, version, TestType);
+			}
+
+			if (TestType.equalsIgnoreCase("networkTest")) {
+				certificationTestPojo = requestInfoService.getCertificationTestFlagData(requestId, version, TestType);
+			}
+			if (TestType.equalsIgnoreCase("HealthTest")) {
+				certificationTestPojo = requestInfoService.getCertificationTestFlagData(requestId, version, TestType);
+			}
+
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-	    return certificationTestPojo;
+		return certificationTestPojo;
 	}
-
 }
