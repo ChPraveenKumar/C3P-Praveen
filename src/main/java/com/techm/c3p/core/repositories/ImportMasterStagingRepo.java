3f7d156e083029638abd98b0296836ba9c92bf9d
Chinthala Praveen Kumar <CK00748386@techmahindra.com>
2022-06-13 16:09:31 +0530

    3909_praveen

--- src/main/java/com/techm/c3p/core/repositories/ImportMasterStagingRepo.java
+++ src/main/java/com/techm/c3p/core/repositories/ImportMasterStagingRepo.java
@@ -20,16 +20,14 @@ public interface ImportMasterStagingRepo extends JpaRepository<ImportMasterStagi
 	@Query("SELECT data FROM ImportMasterStagingEntity data where importId=:importId")
 	List<ImportMasterStagingEntity> getImportStaggingData(@Param("importId") String importId);
 	
-	@Query("SELECT allimport FROM ImportMasterStagingEntity allimport")
-	List<ImportMasterStagingEntity> getAllImport();
+	List<ImportMasterStagingEntity> findAllByStatus(String status);
 	
-	@Query("SELECT myimport FROM ImportMasterStagingEntity myimport where userName like :userName order by executionProcessDate Desc")
-	List<ImportMasterStagingEntity> getMyImport(@Param("userName") String userName);
+	List<ImportMasterStagingEntity> findAllByUserNameAndStatus(String userName, String status);
 	
-	@Query("SELECT count(distinct importId) FROM ImportMasterStagingEntity where userName=:userName and status='Successful'")
+	@Query("SELECT count(distinct importId) FROM ImportMasterStagingEntity where userName=:userName and status='Success'")
 	int myImportCountStatus(@Param("userName") String userName);
 	
-	@Query("SELECT count(distinct importId) FROM ImportMasterStagingEntity where status='Successful'")
+	@Query("SELECT count(distinct importId) FROM ImportMasterStagingEntity where status='Success'")
 	int allImportCountStatus();
 	
 	List<ImportMasterStagingEntity> findByCreatedByOrderByExecutionDateDesc(String userName, Pageable pageable);
