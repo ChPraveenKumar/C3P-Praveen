aca16758adc41dfefb78a00dcebf7cc716f48c97
Gunda Tarun Kumar <gk00754371@techmahindra.com>
2022-06-13 11:11:34 +0000

    Revert "3909_praveen"
    This reverts commit 3f7d156e083029638abd98b0296836ba9c92bf9d

--- src/main/java/com/techm/c3p/core/rest/ImportFile.java
+++ src/main/java/com/techm/c3p/core/rest/ImportFile.java
@@ -532,14 +532,11 @@ public class ImportFile {
 		int myRequestCount, allRequestCount= 0;
 		JSONArray outputArray = new JSONArray();
 		JSONObject object = new JSONObject();
-		String status="Success";
 		try {
 			myRequestCount = importMasterStagingRepo.myImportCountStatus(user);
-			if("all".equalsIgnoreCase(requestType)) {
+			if("all".equalsIgnoreCase(requestType))
 				user = "%";
-				importMasterData =importMasterStagingRepo.findAllByStatus(status);
-			}else	
-			    importMasterData =importMasterStagingRepo.findAllByUserNameAndStatus(user,status);
+			importMasterData =importMasterStagingRepo.getMyImport(user);
 			allRequestCount = importMasterStagingRepo.allImportCountStatus();
 			for (ImportMasterStagingEntity entity : importMasterData) {
 				object = new JSONObject();
@@ -573,7 +570,7 @@ public class ImportFile {
 	@RequestMapping(value = "/getMyDashboardData", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
 	public ResponseEntity discoverDashboard(@RequestParam String user,
-			@RequestParam String requestType,@RequestParam String status) {
+			@RequestParam String requestType) {
 
 		logger.info("Inside getMyDashboardData Service");
 		JSONObject obj = new JSONObject();
@@ -582,7 +579,7 @@ public class ImportFile {
 		JSONObject object = new JSONObject();
 		int myRequestCount, allRequestCount=0;
 		try {
-			importMasterData =importMasterStagingRepo.findAllByUserNameAndStatus(user,status);
+			importMasterData =importMasterStagingRepo.getMyImport(user);
 			myRequestCount = importMasterStagingRepo.myImportCountStatus(user);
 			allRequestCount = importMasterStagingRepo.allImportCountStatus();
 			for (ImportMasterStagingEntity entity : importMasterData) {
